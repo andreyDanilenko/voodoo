60d4fe793ec1d4b8c17fab0de4de826d879d91e4
andreyDanilenko <danilenko.a.g@mail.ru>
2021-11-06 22:16:26 +0300

    search

--- VanillaJS/scripts.js
+++ VanillaJS/scripts.js
@@ -1,6 +1,31 @@
 const POSTS_URL = 'https://jsonplaceholder.typicode.com/posts';
 const USERS_URL = 'https://jsonplaceholder.typicode.com/users';
 
+const postsList = document.querySelector('.posts')
+const searchInput = document.querySelector('.input')
+
+function createPostItem(post, user) {
+    const postItem = document.createElement("li");
+    postItem.classList.add('post');
+
+    const postTitle = document.createElement("h2");
+    postTitle.classList.add('title');
+    postTitle.innerHTML = post.title;
+    postItem.appendChild(postTitle);
+
+    const postDesc = document.createElement("p");
+    postDesc.classList.add('description');
+    postDesc.innerHTML = post.title;
+    postItem.appendChild(postDesc);
+
+    const postAuthor = document.createElement("p");
+    postAuthor.classList.add('author');
+    postAuthor.innerHTML = user[post.userId].name;
+    postItem.appendChild(postAuthor);
+
+    postsList.appendChild(postItem);
+};
+
 function fetchPosts() {
     return fetch(POSTS_URL)
         .then(res => res.json());
@@ -17,17 +42,15 @@ function fetchUsers() {
         });
 }
 
-
 Promise.all([fetchPosts(), fetchUsers()])
     .then(([posts, userMap]) => {
-        document.body.innerHTML =
-            posts.map(post => `
-                <div>
-                    <h3>${post.title} by ${userMap[post.userId].name}</h3>
-                    <p>${post.body}</p>
-                    <br>
-                </div>
-            `).join('');
+        posts.map(post => createPostItem(post, userMap))
+        searchInput.addEventListener('input', (e) => {
+            array = posts.filter(post => userMap[post.userId].name.toLowerCase().includes(e.target.value.toLowerCase()))
+            const postItem = document.querySelectorAll('.post')
+            postItem.forEach(item => {
+                item.remove()
+            })
+            array.map(post => createPostItem(post, userMap))
+        })
     })
-
-
