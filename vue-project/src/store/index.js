871fcffd1fd2c697dcee51f46c4fbc518936ac4d
andreyDanilenko <danilenko.a.g@mail.ru>
2021-11-24 19:09:48 +0300

    creating filtering

--- vue-project/src/store/index.js
+++ vue-project/src/store/index.js
@@ -2,11 +2,47 @@ import { createStore } from 'vuex'
 
 export default createStore({
   state: {
+    posts: [],
+    users: [],
+    searchQuery: '',
   },
+
   mutations: {
+    setFetchPosts(state, posts) {
+      state.posts = posts;
+    },
+
+    setFetchUsers(state, users) {
+      state.users = users;
+    },
+
+    setSearchQuery(state, searchQuery) {
+      state.searchQuery = searchQuery
+    },
   },
+
   actions: {
+    async fetchPosts() {
+      return await fetch('https://jsonplaceholder.typicode.com/posts')
+        .then(res => res.json())
+    },
+
+    async fetchUsers() {
+      return await fetch('https://jsonplaceholder.typicode.com/users')
+        .then(res => res.json())
+        .then(users => {
+          return users.reduce((map, user) => {
+            map[user.id] = user;
+            return map;
+          }, {});
+        });
+    }
   },
-  modules: {
+
+  getters: {
+    searchAuthorPosts(state) {
+      return [...state.posts]
+        .filter(post => state.users[post.userId].name.toLowerCase().includes(state.searchQuery.toLowerCase()))
+    }
   }
 })
--- vue-project/src/views/Home.vue
+++ vue-project/src/views/Home.vue
@@ -1,7 +1,104 @@
 <template>
-  <div class="home">sdsd</div>
+  <div class="home">
+    <div class="home__search">
+      <input
+        class="home__search-input"
+        type="text"
+        @input="serachAuthorPosts"
+      />
+    </div>
+    <div class="home__posts">
+      <div v-for="post in searchAuthorPosts" :key="post" class="home__post">
+        <h2 class="home__post-title">{{ post.title }}</h2>
+        <div class="home__post-body">{{ post.body }}</div>
+        <div class="home__post-author">{{ users[post.userId].name }}</div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
-export default {};
+import { mapState, mapGetters, mapActions, mapMutations } from "vuex";
+
+export default {
+  computed: {
+    ...mapGetters(["searchAuthorPosts"]),
+    ...mapState({
+      users: (state) => state.users,
+    }),
+  },
+
+  methods: {
+    ...mapActions(["fetchPosts", "fetchUsers"]),
+    ...mapMutations(["setFetchPosts", "setFetchUsers", "setSearchQuery"]),
+
+    serachAuthorPosts(evt) {
+      this.setSearchQuery(evt.target.value);
+    },
+  },
+
+  mounted() {
+    Promise.all([this.fetchPosts(), this.fetchUsers()]).then(
+      ([posts, userMap]) => {
+        this.setFetchPosts(posts);
+        this.setFetchUsers(userMap);
+      }
+    );
+  },
+};
 </script>
+
+<style lang="scss">
+.home {
+  max-width: 1280px;
+  padding: 0 20px;
+  margin: 0 auto;
+
+  &__search-input {
+    box-sizing: border-box;
+    width: 100%;
+    padding: 10px;
+    border: 1px solid #4682b4;
+    font-size: 30px;
+  }
+
+  &__posts {
+    display: flex;
+    justify-content: space-between;
+
+    flex-wrap: wrap;
+  }
+
+  &__post {
+    box-sizing: border-box;
+    margin-top: 15px;
+    width: 100%;
+    border: 1px solid #4682b4;
+    padding: 20px;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+
+    @media (min-width: 660px) {
+      width: calc((100% / 2) - 30px);
+    }
+
+    @media (min-width: 960px) {
+      width: calc((100% / 3) - 30px);
+    }
+  }
+
+  &__post-title {
+    margin: 0;
+  }
+
+  &__post-body {
+    margin-top: 10px;
+  }
+
+  &__post-author {
+    margin-top: 10px;
+    font-weight: 600;
+  }
+}
+</style>
